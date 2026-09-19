8715883d2e8c25a005b1a02a46ca7c4b3d654103
Priyanshu Kumar <154836030+priy1105@users.noreply.github.com>
2026-09-19 23:55:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ SHEET_LINK = "https://docs.google.com/spreadsheets/d/1W7tGtN3weAsBQ9fmT42Swjte1S
 
 st.set_page_config(page_title="BrightChamps Task - By Priyanshu", page_icon="🚀", layout="centered")
 
-st.subheader("🚀 Founder's Office: BrightChamps")
-st.markdown("Assignment - Priyanshu - B.Tech., NITK Surathkal")
+st.subheader("🚀 Founder's Office Assignment: BrightChamps")
+st.subheader("Priyanshu - B.Tech., NITK Surathkal")
 
 st.divider()
 st.subheader("Step 1: Stage the Google Sheets CRM")
